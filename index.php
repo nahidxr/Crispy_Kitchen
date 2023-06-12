4402db6467709635b7074c13a4efed1d32dcb7d4
Md Nahidul Islam <130117582+mdnahidulislam1@users.noreply.github.com>
2023-06-12 13:48:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -213,7 +213,7 @@ https://www.tooplate.com/view/2129-crispy-kitchen
 
                        <?php
 
-                        $link = mysqli_connect('172.20.1.101', 'ecomuser', 'ecompassword', 'ecomdb','3306');
+                        $link = mysqli_connect('nahid-db.czk1aergjpmh.eu-west-2.rds.amazonaws.com', 'admin', '12345678', 'crispydb','3306');
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
